1d494efd3658be267a7063cf9f79343b3577d0a9
Vini Queiroz <viniderp@gmail.com>
2021-07-03 00:49:50 -0300

    chat broadcast

---
+++ src/components/Media/Player/Player.js
@@ -0,0 +1,100 @@
+import { useRef, useState, useEffect } from 'react';
+
+import classes from './Player.module.css';
+
+const Player = (props) => {
+  const [fillWidth, setFillWidth] = useState();
+  const [playerIcon, setPlayerIcon] = useState('fas fa-play');
+  const videoRef = useRef('');
+  const barRef = useRef('');
+  const volRef = useRef(1);
+
+  useEffect(() => {
+    videoRef.current.load();
+  }, [props.source]);
+
+  const playHandler = () => {
+    if (!props.source) return;
+
+    if (videoRef.current.paused) {
+      setPlayerIcon('fas fa-pause');
+      videoRef.current.play();
+    } else {
+      setPlayerIcon('fas fa-play');
+      videoRef.current.pause();
+    }
+  };
+
+  const seekHandler = (e) => {
+    const barX0 = barRef.current.getBoundingClientRect().left;
+    const barX1 =
+      barRef.current.getBoundingClientRect().left + barRef.current.offsetWidth;
+
+    if (e.clientX > barX0 && e.clientX < barX1) {
+      const absProgress = e.clientX - barX0;
+      const relativeProgress = absProgress / barRef.current.offsetWidth;
+      videoRef.current.currentTime =
+        relativeProgress * videoRef.current.duration;
+      videoRef.current.play();
+      // const action = receivedAction || {
+      //   id: Math.random().toString().slice(16),
+      //   user: myId,
+      //   date: Date.now(),
+      // };
+      // socket.emit('play', videoRef.current.currentTime, Date.now(), action);
+      // receivedAction = null;
+    }
+  };
+
+  const volumeChangeHandler = () => {
+    videoRef.current.volume = volRef.current.value;
+  };
+
+  const timeUpdateHandler = () => {
+    const progress = videoRef.current.currentTime / videoRef.current.duration;
+    setFillWidth(progress * barRef.current.offsetWidth + 'px');
+  };
+
+  return (
+    <div className={classes['player-container']}>
+      <video
+        ref={videoRef}
+        onTimeUpdate={timeUpdateHandler}
+        id='my-video'
+        className=''
+      >
+        <source id='source-video' src={props.source} type='video/mp4' />
+      </video>
+      <div className={classes['player-controls']}>
+        <div
+          onClick={seekHandler}
+          ref={barRef}
+          className={classes['progress-bar']}
+        >
+          <div
+            style={{ width: fillWidth }}
+            className={classes['progress-bar-fill']}
+          ></div>
+        </div>
+        <div className={classes['player-buttons']}>
+          <button onClick={playHandler} className='my-btn-play'>
+            <i className={playerIcon}></i>
+          </button>
+          <div className={classes['control-volume']}>
+            <i className='fas fa-volume-down'></i>
+            <input
+              onChange={volumeChangeHandler}
+              type='range'
+              step='0.05'
+              min='0'
+              max='1'
+              ref={volRef}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Player;
---
+++ src/components/Media/VideoForm/VideoForm.js
@@ -0,0 +1,56 @@
+import { useRef, useState } from 'react';
+import classes from './VideoForm.module.css';
+
+const VideoForm = (props) => {
+  const inputRef = useRef('');
+  const [bg, setBg] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const file = inputRef.current.files[0];
+
+    if (!file) return;
+
+    props.onUpload(file);
+
+    // video.insertAdjacentHTML(
+    //   'beforebegin',
+    //   `
+    //   <h4>You uploaded: ${inputVideo.files[0].name}</h4>
+    // `
+    // );
+
+    // socket.emit('uploaded', myId, file.name);
+
+    // video.load();
+  };
+
+  // useEffect(() => {
+
+  // })
+
+  // setTimeout(() => {
+  //   const bgColor = `rgb(${Math.random() * 256},${Math.random() * 256},${
+  //     Math.random() * 256
+  //   })`;
+  //   // setBg(bgColor);
+  // }, 1000);
+
+  return (
+    <form className={classes['video-form']}>
+      <input
+        ref={inputRef}
+        className='input-video'
+        type='file'
+        accept='video/*'
+        required
+      />
+      <button onClick={submitHandler} className='submit' type='submit'>
+        Special Button made by a Parrot
+      </button>
+    </form>
+  );
+};
+
+export default VideoForm;
